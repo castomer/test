f1c0ac693878455fc72cf2c5bcd38b9c9b045fbd
Roman <evralev.us@gmail.com>
2023-08-24 13:47:57 +0300

    test

--- app/pages/homePage/homePage.js
+++ app/pages/homePage/homePage.js
@@ -1,22 +1,17 @@
+const { expect } = require("@playwright/test");
+const { BasePage } = require("../base/basePage");
 
-const { expect } = require('@playwright/test');
-const { BasePage } = require('../base/basePage');
-
-exports.HomePage = class HomePage extends BasePage{
-
+exports.HomePage = class HomePage extends BasePage {
   constructor(page) {
-   super(page);
-   this.basePage = new BasePage(page);
-   this.frameName = 'XBGSFAMB'
+    super(page);
+    this.basePage = new BasePage(page);
+    this.frameName = "XBGSFAMB";
   }
 
   // Проверка наличия элемента в футере
   async goToFrom() {
     console.log("____________Переход к форме пожертвования____________");
-    await this.basePage.frameClick(this.frameName, this.textLoc('Give Now')) // клик элементу внутри фрейма
-    await this.basePage.pageLoad() // ожиание загрузки 
+    await this.basePage.frameClick(this.frameName, this.textLoc("Give Now")); // клик элементу внутри фрейма
+    await this.basePage.pageLoad(); // ожиание загрузки
   }
-
-
 };
-
